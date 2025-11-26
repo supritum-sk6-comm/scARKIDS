b622a517fe0bd5ab53d99da7920ced4780b68861
supritum-sk6 <supritum21@itk.ac.in>
2025-11-27 01:25:39 +0530

    debug_2. src/utils/logger.py

--- src/utils/logger.py
+++ src/utils/logger.py
@@ -1,22 +1,39 @@
 import logging
 
 
-
-# ---- Logger Class ----
 class Logger:
-    def __init__(self, name=__name__):
-        self.logger = logging.getLogger(name)
-        handler = logging.StreamHandler()
-        formatter = logging.Formatter("%(asctime)s | %(levelname)s | %(name)s | %(message)s")
-        handler.setFormatter(formatter)
-        handler.setLevel(logging.INFO)
-        self.logger.addHandler(handler)
-        self.logger.setLevel(logging.INFO)
-    def debug(self, msg):
+    """Project-wide logger helper."""
+
+    _configured = set()
+
+    @classmethod
+    def get_logger(cls, name: str = __name__) -> logging.Logger:
+        """Return a configured logger with the given name."""
+        logger = logging.getLogger(name)
+        if name not in cls._configured:
+            handler = logging.StreamHandler()
+            formatter = logging.Formatter(
+                "%(asctime)s | %(levelname)s | %(name)s | %(message)s"
+            )
+            handler.setFormatter(formatter)
+            handler.setLevel(logging.INFO)
+            logger.addHandler(handler)
+            logger.setLevel(logging.INFO)
+            cls._configured.add(name)
+        return logger
+
+    # Optional instance-style wrapper if you ever need it
+    def __init__(self, name: str = __name__):
+        self.logger = self.get_logger(name)
+
+    def debug(self, msg: str):
         self.logger.debug(msg)
-    def info(self, msg):
+
+    def info(self, msg: str):
         self.logger.info(msg)
-    def warning(self, msg):
+
+    def warning(self, msg: str):
         self.logger.warning(msg)
-    def error(self, msg):
+
+    def error(self, msg: str):
         self.logger.error(msg, exc_info=True)
